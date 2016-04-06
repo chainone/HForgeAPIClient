eef0b7dec7677fb3d55c99b07dd99a181d7ecc39
YoungChen <chainone@gmail.com>
2016-04-07 00:11:15 +0800

    added OSS Bucket creation API

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import ViewDataApi
 
+import Control.Applicative
+import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Except
+import Servant.Client
+import Network.HTTP.Client (newManager, Manager)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+
+oneOxygenClient = OxygenClientInfo "xxx" "xx"
+oneOSSBucketInfo = OSSBucketInfo "mybucketwwww" Temporary
+
 main :: IO ()
-main = putStrLn "test"
+main = print =<< uselessNumbers
+
+uselessNumbers :: IO (Either ServantError ())
+uselessNumbers = runExceptT $ do
+   let baseURL = BaseUrl Https "developer.api.autodesk.com" 443 ""
+   manager <- liftIO $ newManager tlsManagerSettings
+   liftIO $ putStrLn "Fetching token..."
+   token <- getToken oneOxygenClient manager baseURL
+   liftIO . putStrLn $ "Token is " ++ access_token token
+   liftIO . putStrLn $ "Creating OSS bucket..."
+   bucket <- createOSSBucket (Just $ tokenHeaderValue token) oneOSSBucketInfo manager baseURL
+   liftIO . putStrLn $ "Bucket " ++ bucketKey bucket ++ " created"
--- src/ViewDataApi/ViewDataApi.hs
+++ src/ViewDataApi/ViewDataApi.hs
@@ -4,64 +4,90 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module ViewDataApi
-   ( adskAPI
+   ( viewDataAPI
+   , OxygenClientInfo(..)
+   , OxygenClientToken(..)
+   , OSSObjectPolicy(..)
+   , OSSBucketInfo(..)
+   , getToken
+   , createOSSBucket
+   , tokenHeaderValue
    ) where
 
 import Control.Applicative
 import Control.Monad
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Except
-import Data.Aeson
+import qualified Data.Aeson as J
 import Data.Monoid
 import Data.Proxy
 import Data.Text (Text)
 import GHC.Generics
 import Servant.API
 import Servant.Client
-import Network.HTTP.Client (newManager, Manager)
-import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Network.HTTP.Client (Manager)
 
 import qualified Data.Text    as T
 import qualified Data.Text.IO as T
 
+-- 1. Auth API
 
-oneAdskClient = AdskClientInfo "xxx" "xxx"
-
-data AdskClientInfo = AdskClientInfo{
-      adskClientId :: Text
-   ,  adskClientSecret :: Text
+data OxygenClientInfo = OxygenClientInfo{
+      oxygenClientId :: Text
+   ,  oxygenClientSecret :: Text
 } deriving (Eq, Show)
 
-instance ToFormUrlEncoded AdskClientInfo where
-   toFormUrlEncoded clientInfo = [("client_id", adskClientId clientInfo), ("client_secret", adskClientSecret clientInfo), ("grant_type", "client_credentials")]
+instance ToFormUrlEncoded OxygenClientInfo where
+   toFormUrlEncoded clientInfo = [("client_id", oxygenClientId clientInfo), ("client_secret", oxygenClientSecret clientInfo), ("grant_type", "client_credentials")]
 
-data AdskClientToken = AdskClientToken{
+data OxygenClientToken = OxygenClientToken{
       token_type :: String
    ,  expires_in :: Int
    ,  access_token :: String
 }deriving (Eq, Show, Generic)
 
-instance FromJSON AdskClientToken
+instance J.FromJSON OxygenClientToken
+
+tokenHeaderValue :: OxygenClientToken -> String
+tokenHeaderValue token = token_type token ++ " " ++ access_token token
+
+-- 2. OSS Create bucket
+data OSSObjectPolicy = Transient | Temporary | Persistent
+   deriving (Eq, Show)
+
+instance J.ToJSON OSSObjectPolicy where
+   toJSON Transient = J.String "transient"
+   toJSON Temporary = J.String "temporary"
+   toJSON Persistent = J.String "persistent"
+
+instance J.FromJSON OSSObjectPolicy where
+   parseJSON (J.String "transient") = return Transient
+   parseJSON (J.String "temporary") = return Temporary
+   parseJSON (J.String "persistent") = return Persistent
+   parseJSON _ = empty
 
 
+data OSSBucketInfo = OSSBucketInfo{
+      bucketKey :: String
+   ,  policyKey :: OSSObjectPolicy
+}deriving (Eq, Show, Generic)
 
-type ADSKAPI = "authentication" :> "v1" :> "authenticate" :> ReqBody '[FormUrlEncoded] AdskClientInfo :> Post '[JSON] AdskClientToken
-adskAPI :: Proxy ADSKAPI
-adskAPI = Proxy
+instance J.ToJSON OSSBucketInfo
 
+instance J.FromJSON OSSBucketInfo
 
+---------------------------
+-- API Declaration
+---------------------------
+type OxygenAuth = "authentication" :> "v1" :> "authenticate" :> ReqBody '[FormUrlEncoded] OxygenClientInfo :> Post '[JSON] OxygenClientToken
+type OSSCreateBucket = "oss" :> "v2" :> "buckets" :> Header "Authorization" String :> ReqBody '[JSON] OSSBucketInfo :> Post '[JSON] OSSBucketInfo
 
-getToken ::  AdskClientInfo -> Manager -> BaseUrl -> ExceptT ServantError IO AdskClientToken
+type ViewDataAPI = OxygenAuth :<|> OSSCreateBucket
 
-getToken = client adskAPI
+viewDataAPI :: Proxy ViewDataAPI
+viewDataAPI = Proxy
 
+getToken ::  OxygenClientInfo -> Manager -> BaseUrl -> ExceptT ServantError IO OxygenClientToken
+createOSSBucket ::  Maybe String -> OSSBucketInfo -> Manager -> BaseUrl -> ExceptT ServantError IO OSSBucketInfo
 
--- main :: IO ()
--- main = print =<< uselessNumbers
---
--- uselessNumbers :: IO (Either ServantError ())
--- uselessNumbers = runExceptT $ do
---    let baseURL = BaseUrl Https "developer.api.autodesk.com" 443 ""
---    manager <- liftIO $ newManager tlsManagerSettings
---    token <- getToken oneAdskClient manager baseURL
---    liftIO . putStrLn $ show token
+getToken :<|> createOSSBucket = client viewDataAPI
